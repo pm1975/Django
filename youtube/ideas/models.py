89cb4199c01665284e7ccd7b5f5de8737d752140
PM <pm1975@windowslive.com>
2022-04-04 13:58:34 +0200

    first commit

---
+++ youtube/ideas/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+IDEA_STATUS = (
+    ('pending', 'Waiting for review'),
+    ('accepted', 'Accepted'),
+    ('done', 'Done'),
+    ('rejected', 'Rejected')
+)
+
+class Idea(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    youtube_url = models.URLField(null=True, blank=True)
+    status = models.CharField(choices=IDEA_STATUS, max_length=30, default='pending')
+
+
+class Vote(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    reason = models.TextField()
+
